5c3bda7d8878e0cc69686cd4c2f1a208d824c9e5
eandr127 <rebl10@hotmail.com>
2014-01-12 17:52:35 -0500

    Fixed CommandListener not parsing CommandList[] correctly

--- Head-Game/src/Commands/CommandStop.java
+++ Head-Game/src/Commands/CommandStop.java
@@ -11,19 +11,7 @@ public class CommandStop{
      * @param cmdArgs the command arguments
      */
     public CommandStop(String[] cmdArgs){
-        if(cmdArgs.length != 1){
-            if (cmdArgs.length < 0){
-                System.out.println("Not enough args");
-                System.out.println("Usage: /stop");
-            }
-            else {
-                System.out.println("Too many args");
-                System.out.println("Usage: /stop");
-            }
-        }
-        else {
-            System.out.println("Stoping game.");
-            System.exit(0);
-        }
+        System.out.println("Stoping game.");
+        System.exit(0);
     }
 }
--- Head-Game/src/headGame/CommandListener.java
+++ Head-Game/src/headGame/CommandListener.java
@@ -15,33 +15,35 @@ public class CommandListener {
     public CommandListener(){
         
         CommandList commands = new CommandList(); 
+        String inputString;
         while(true) {
             Scanner sc = new Scanner(System.in);
-            String inputString = sc.nextLine(); // you could swith it with nextInt(), nextFloat() etc based on yoir need
-            inputString.toLowerCase();
-            
-            for(int i =0; i == commands.CommandList().length -1; i++){
-                if(inputString.startsWith(commands.CommandList()[i])) {
-                    switch(commands.CommandList()[i]){
-                        case "/stop":
-                            new CommandStop(commands.CommandList());
-                        break;
-                        case "/speed ":
-                            new CommandSpeed(commands.CommandList());
-                        break;
-                        case "/speed":
-                            new CommandSpeedUsage(commands.CommandList());
-                        break;
-                        case "/tp ":
-                            new CommandTP(commands.CommandList());
-                        break;
-                        case "/tp":
-                            new CommandTPUsage(commands.CommandList());
-                        break;
-                    }        
-                }
-                else {
-                  System.out.println("Unknown command.");
+            inputString = sc.nextLine(); // you could swith it with nextInt(), nextFloat() etc based on yoir need
+            if(inputString != null) {
+                for(int i = 0; i <= commands.CommandList().length -1; i++){
+                    if(inputString.startsWith(commands.CommandList()[i])) {
+                        switch(commands.CommandList()[i]){
+                            case "/stop":
+                                new CommandStop(commands.CommandList());
+                            break;
+                            case "/speed ":
+                                new CommandSpeed(commands.CommandList());
+                            break;
+                            case "/speed":
+                                new CommandSpeedUsage(commands.CommandList());
+                            break;
+                            case "/tp ":
+                                new CommandTP(commands.CommandList());
+                            break;
+                            case "/tp":
+                                new CommandTPUsage(commands.CommandList());
+                            break;
+                        }        
+                    }
+                    
+                    else {
+                        System.out.println("Unknown command.");
+                    }
                 }
             }
         }
